e868b31efcc5f3a77c0124ef25a4a7cbfef2b6e2
oni-shiro <sinchan.saha99@gmail.com>
2024-02-07 23:00:05 +0530

    Initial Project Commit

---
+++ dev-inktown/src/main/java/com/dev/inktown/DevInktownApplication.java
@@ -0,0 +1,13 @@
+package com.dev.inktown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevInktownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevInktownApplication.class, args);
+	}
+
+}
